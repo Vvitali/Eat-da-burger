77b83a0a6d12af4a03ffe610ac628a7e3da37e3f
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-12-12 01:25:04 -0500

    basic view and handlebars

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var express = new require('express')();
+var app = new require('express')();
 var mysql = require('mysql');
 var bodyparser = require('body-parser');
 
 
 app.set("view engine", "hbs");
 
-app.use("/")
+app.get("/:par_name?", function(request, response){
 
+	console.log("// %s", request.params.par_name);
+	response.render("./index.hbs", {
+		name: request.params.par_name
+	})
+})
+
+app.listen(8080);
