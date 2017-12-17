6fbbbbd84d84fd72747fab47cbdeefd3c28d21e2
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-12-16 22:10:33 -0500

    new database

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 console.log("mysql");
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
+	host     : 'tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user     : 'pup3s1nhud56obne',
+	password : 'y7m2cvcxezq6xwbz',
 	database : 'burger_db'
 });
 connection.connect(function(err){
